1061075913fc75fc9e7ff7a4ca7625c8c7105999
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-01-13 05:26:15 +0530

    Plaintexts added on top of file by guessing intelligently by defining the targets and ciphertexts appropriately.The text to be found out is called target and the others are referred to as ciphertexts

--- Many Time Pass Hack/manytimepass.py
+++ Many Time Pass Hack/manytimepass.py
@@ -6,9 +6,17 @@ Created on Mon Aug 20 15:09:38 2018
 """
 import codecs
 
+# ciphertext[0] = We can factor the number 15 with quantum computers. We can also factor the number 15 with a dog trained to bark three times - Robert Harley
+# ciphertext[1] = Euler would probably enjoy that now his theorem becomes a corner stone of crypto - Annonymous on Euler's theorem
+# ciphertext[2] = The nice thing about Keeyloq is now we cryptographers can drive a lot of fancy cars - Dan Boneh
+# ciphertext[3] = The ciphertext produced by a weak encryption algorithm looks as good as ciphertext produced by a strong encryption algorithm - Philip Zimmermann
+# ciphertext[4] = You don't want to buy a set of car keys from a guy who specializes in stealing cars - Marc Rotenberg commenting on Clipper
+# ciphertext[5] = There are two types of cryptography - that which will keep secrets safe from your little sister, and that which will keep secrets safe from your government - Bruce Schneier
+# ciphertext[6] = There are two types of cryptography: one that allows the Government to use brute force to break the code and one that requires the Government to use brute force to break you
+# ciphertext[7] = We can see the point where the chip is unhappy if a wrong bit is sent and consumes more power from the environment - Adi Shamir
 # ciphertext[8] = A (private key) encryption scheme states 3 algorithms, namely a procedure for generating keys,a procedure for encrypting, and a procedure for decrypting.
-# ciphertext[9] =
-# target = The secret message is : When using a stream cipher never use the key more than once.
+# ciphertext[9] = The Concise OxfordDictionary (2006) defines crypto as the art of writing or solving code
+# target = The secret message is: When using a stream cipher, never use the key more than once
 
 ciphertexts={}
 ciphertexts[0] = "315c4eeaa8b5f8aaf9174145bf43e1784b8fa00dc71d885a804e5ee9fa40b16349c146fb778cdf2d3aff021dfff5b403b510d0d0455468aeb98622b137dae857553ccd8883a7bc37520e06e515d22c954eba5025b8cc57ee59418ce7dc6bc41556bdb36bbca3e8774301fbcaa3b83b220809560987815f65286764703de0f3d524400a19b159610b11ef3e"
@@ -20,8 +28,8 @@ ciphertexts[5] = "32510bfbacfbb9befd54415da243e1695ecabd58c519cd4bd2061bbde24eb7
 ciphertexts[6] = "32510bfbacfbb9befd54415da243e1695ecabd58c519cd4bd90f1fa6ea5ba47b01c909ba7696cf606ef40c04afe1ac0aa8148dd066592ded9f8774b529c7ea125d298e8883f5e9305f4b44f915cb2bd05af51373fd9b4af511039fa2d96f83414aaaf261bda2e97b170fb5cce2a53e675c154c0d9681596934777e2275b381ce2e40582afe67650b13e72287ff2270abcf73bb028932836fbdecfecee0a3b894473c1bbeb6b4913a536ce4f9b13f1efff71ea313c8661dd9a4ce"
 ciphertexts[7] = "315c4eeaa8b5f8bffd11155ea506b56041c6a00c8a08854dd21a4bbde54ce56801d943ba708b8a3574f40c00fff9e00fa1439fd0654327a3bfc860b92f89ee04132ecb9298f5fd2d5e4b45e40ecc3b9d59e9417df7c95bba410e9aa2ca24c5474da2f276baa3ac325918b2daada43d6712150441c2e04f6565517f317da9d3"
 ciphertexts[8] = "271946f9bbb2aeadec111841a81abc300ecaa01bd8069d5cc91005e9fe4aad6e04d513e96d99de2569bc5e50eeeca709b50a8a987f4264edb6896fb537d0a716132ddc938fb0f836480e06ed0fcd6e9759f40462f9cf57f4564186a2c1778f1543efa270bda5e933421cbe88a4a52222190f471e9bd15f652b653b7071aec59a2705081ffe72651d08f822c9ed6d76e48b63ab15d0208573a7eef027"
-target = "466d06ece998b7a2fb1d464fed2ced7641ddaa3cc31c9941cf110abbf409ed39598005b3399ccfafb61d0315fca0a314be138a9f32503bedac8067f03adbf3575c3b8edc9ba7f537530541ab0f9f3cd04ff50d66f1d559ba520e89a2cb2a83"
-ciphertexts[9] = "32510ba9babebbbefd001547a810e67149caee11d945cd7fc81a05e9f85aac650e9052ba6a8cd8257bf14d13e6f0a803b54fde9e77472dbff89d71b57bddef121336cb85ccb8f3315f4b52e301d16e9f52f904"
+ciphertexts[9] = "466d06ece998b7a2fb1d464fed2ced7641ddaa3cc31c9941cf110abbf409ed39598005b3399ccfafb61d0315fca0a314be138a9f32503bedac8067f03adbf3575c3b8edc9ba7f537530541ab0f9f3cd04ff50d66f1d559ba520e89a2cb2a83"
+target = "32510ba9babebbbefd001547a810e67149caee11d945cd7fc81a05e9f85aac650e9052ba6a8cd8257bf14d13e6f0a803b54fde9e77472dbff89d71b57bddef121336cb85ccb8f3315f4b52e301d16e9f52f904"
 
 # xor two strings of different lengths
 def strxor(a, b):
@@ -77,21 +85,14 @@ for j in range(maxLength):
                 list1.append(i[j])
         except:
             continue
-        #
-        # elif (ord(i[j]) >= 48 or ord(i[j]) <= 57):
-        #     list1.append(i[j])
-        # elif
-    # print(list1)
-    list2 = list1[:]
-    distinct = 0
+
     counted = []
-    for element in list2:
+    for element in list1:
         if element in counted:
             continue
         else:
             counted.append(element)
-    # print(list1)
-    if len(counted) > 4 or list1 == []:
+    if len(counted) > 3 or list1 == []:
         s4+= " "
     else:
         s4 += max(list1, key = list1.count)
