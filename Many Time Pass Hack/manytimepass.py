6837737358ba02e3facdb7f742dbf3d91b680ad8
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-01-13 05:28:57 +0530

    Functions to be added and code to be restructured.Also,comments are to be added.

--- Many Time Pass Hack/manytimepass.py
+++ Many Time Pass Hack/manytimepass.py
@@ -18,7 +18,7 @@ import codecs
 # ciphertext[9] = The Concise OxfordDictionary (2006) defines crypto as the art of writing or solving code
 # target = The secret message is: When using a stream cipher, never use the key more than once
 
-ciphertexts={}
+ciphertexts = {}
 ciphertexts[0] = "315c4eeaa8b5f8aaf9174145bf43e1784b8fa00dc71d885a804e5ee9fa40b16349c146fb778cdf2d3aff021dfff5b403b510d0d0455468aeb98622b137dae857553ccd8883a7bc37520e06e515d22c954eba5025b8cc57ee59418ce7dc6bc41556bdb36bbca3e8774301fbcaa3b83b220809560987815f65286764703de0f3d524400a19b159610b11ef3e"
 ciphertexts[1] = "234c02ecbbfbafa3ed18510abd11fa724fcda2018a1a8342cf064bbde548b12b07df44ba7191d9606ef4081ffde5ad46a5069d9f7f543bedb9c861bf29c7e205132eda9382b0bc2c5c4b45f919cf3a9f1cb74151f6d551f4480c82b2cb24cc5b028aa76eb7b4ab24171ab3cdadb8356f"
 ciphertexts[2] = "32510ba9a7b2bba9b8005d43a304b5714cc0bb0c8a34884dd91304b8ad40b62b07df44ba6e9d8a2368e51d04e0e7b207b70b9b8261112bacb6c866a232dfe257527dc29398f5f3251a0d47e503c66e935de81230b59b7afb5f41afa8d661cb"
@@ -39,8 +39,8 @@ def strxor(a, b):
        return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b[:len(a)])])
 
 
-target=codecs.decode(target,"hex")
-s=""
+target = codecs.decode(target,"hex")
+s = ""
 for i in target:
     s += chr(i)
 answers = []
